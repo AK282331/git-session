6280b2d9efef7f3892185b9d1544cedb237496b3
Abhinav Khandelwal <khandelwalabhinav791@gmail.com>
2025-11-25 10:48:22 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return{
+        "status":"SUCCESS",
+        "data": {
+            "regno":24122002,
+            "name":"Abhinav",
+            "email":"abhinav@gmail.com"
+        }
+    }
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
